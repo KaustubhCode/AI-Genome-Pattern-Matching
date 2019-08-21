04b903077e2aa69521b37922cd2775225fd548c5
Abhyuday Bhartiya <bhartiyabhyuday@gmail.com>
2019-08-21 22:40:08 +0530

    Initial input trial

--- ass1.cpp
+++ ass1.cpp
@@ -10,17 +10,38 @@ void int main(){
 
 	cout << "Reading from the file" << endl; 
 	string str;
-	vector<string> strlist;
-
-	int time;
+	float time;
 	int m;
 	int k;
-	vector<string> genes;
-	int[][] costmap;
-
-	while(getline(infile,str)){
-
+	string s[k] ;
+	char units[m] ;
+	char temp ;
+	int costmap[m+1][m+1];
+	int cc ;
+	infile >> time ;
+	infile >> m ;
+	for(int i = 0 ; i < m ; i++){
+	    infile >> units[i] ;
+	    infile >> temp ;
 	}
-
+	infile >> k ;
+    for(int i = 0 ; i < k ; i++){
+        infile >>  s[i] ;
+    }
+    infile >> cc ;
+	for(int i = 0 ; i < = m ; i++){
+	    for(int j = 0 ; j <= m ; j++){
+	        infile >> costmap[i][j] ;
+	    }
+	}
+    infile >> temp ;
+	cout << temp ;
+	cout << "Recieved Costs" << endl ;
+    for(int i = 0 ; i < = m ; i++){
+        for(int j = 0 ; j <= m ; j++){
+            cout << costmap[i][j] << " " ;
+        }
+        cout << endl ;
+    }
 	return 0;
 }
