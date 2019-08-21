1b221ad30de9033796d269cc262f104380cfa22e
Kaustubh Prakash <mt1160647@iitd.ac.in>
2019-08-21 22:07:02 +0530

    First commit

---
+++ ass1.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void int main(){
+	ifstream infile; 
+	infile.open("input.txt"); 
+
+	cout << "Reading from the file" << endl; 
+	string str;
+	vector<string> strlist;
+
+	int time
+	while(getline(infile,str)){
+
+	}
+
+	return 0;
+}
