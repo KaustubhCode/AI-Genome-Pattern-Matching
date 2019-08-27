5b9d4813d08a3c95a112b32b67d716a5c3fc8797
Kaustubh <kaustubh698@gmail.com>
2019-08-28 04:27:43 +0530

    Created state structure and added simple hill climbing algo

--- ass1.cpp
+++ ass1.cpp
@@ -1,49 +1,139 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 #include <string>
+#include <unordered_map>
+#include <ctime>
 
 using namespace std;
 
+//Global variable
+vector<vector<int> > globalcosts;		// Cost matrix
+vector<char> units; 					//{'A','T','G','C','_'};
+unordered_map<char, int> umap; 			//{'A':0,'T':1,'G':2,'C':3,'_':4};
+int cc;									// Cost of inserting '_'
+time_t start_time;						// Starting time
+float total_time;						// Time the program should run
+int k;									// Number of strings
+int m;									// Number of units (excluding '_')
+int maxlen;								// Max Length of strings given
+
+//State Class
 class state{
-public:
-	int f;
-	int g;
-	vector<int> indices ;
+	public:
+		int score;
+		vector<string> all_seq;
+		int n; //Length of strings
 
-	state(){
-		f = 0 ;
-		g = 0 ;
+		state(int length_strings){
+			n = length_strings;
+			score = 0;
+		}
+
+		state(int length_strings, vector<string> new_seq){
+			n = length_strings;
+			all_seq = new_seq;
+			score = 0;
+			compute_cost();
+		}
+
+		void add(string seq){
+			all_seq.push_back(seq);
+		}
+
+		void add(state prev){
+			all_seq = prev.all_seq;
+		}
+
+		void add_score(int new_score){
+			score = new_score;
+		}
+
+		void compute_cost(){
+			score = 0;
+			for (int i = 0; i < n; i++){
+				for (int j = 0; j < k; j++){
+					if (all_seq.at(j).at(i) == '_'){
+						score += cc;
+					}
+					for (int p = j+1; p < k; p++){
+						score += globalcosts.at(umap[all_seq.at(j).at(i)]).at(umap[all_seq.at(p).at(i)]);
+					}
+				}
+			}
+		}
+
+		void print_state(){
+			for (int i = 0; i < k; i++){
+				cout << all_seq.at(i) << " ";
+			}
+			cout << endl;
+			cout << "Score is: " << score << endl;  
+		}
+};
+
+state greedy_choose(state current, int ind){
+	string str = current.all_seq.at(ind);
+	int min_score = current.score;
+	state best_state = current;
+	for (int i = 0; i < str.size();i++){
+		if (str.at(i) == '_'){
+			if (i-1 >= 0 && str.at(i-1) != '_'){
+				swap(str[i-1],str[i]);
+				vector<string> new_seq = current.all_seq;
+				new_seq.at(ind) = str;
+				state new_state(current.n, new_seq);
+				if (new_state.score <= min_score){
+					best_state = new_state;
+					min_score = new_state.score;
+				}
+				swap(str[i-1],str[i]);
+			}
+			if (i+1 < str.size() && str.at(i+1) != '_'){
+				swap(str[i+1],str[i]);
+				vector<string> new_seq = current.all_seq;
+				new_seq.at(ind) = str;
+				state new_state(current.n, new_seq);
+				if (new_state.score <= min_score){
+					best_state = new_state;
+					min_score = new_state.score;
+				}
+				swap(str[i+1],str[i]);	
+			}
+		}
 	}
-	state(int x,int y,vector<int> v){
-		f = x ;
-		g = y ;
-		indices = v ;
+	return best_state;
+}
+
+state greedy_local_search(state current){
+	//Find next best state
+	int count = 0;
+	while (true){
+		// cout << "Working:" << count << endl;
+		state next = greedy_choose(current,count);
+		next.print_state();
+
+		if (next.score > current.score){
+			return current;
+		}
+		else{
+			current = next;
+		}
+		count = (count+1)%k;
 	}
-	// vector<vector<int>>	expand(){
-	// 	vector<vector<int>>
-	// }
-
-
 }
 
 int main(){
+
+	start_time = time(NULL);
+
 	ifstream infile; 
 	infile.open("input.txt"); 
-
 	cout << "Reading from the file" << endl; 
-	string str;
-	float time;
-	int m;
-	int k;
-	int cc ;
-	infile >> time ;
+	infile >> total_time ;
 	infile >> m ;
-
-
-	char units[m] ;
-	int costmap[m+1][m+1];
-	char temp;
-
+	
+	string str;
 	getline(infile,str);
 	getline(infile,str);
 
@@ -53,43 +143,59 @@ int main(){
 		if(str.at(i) == ' ' || str.at(i) == ','){
 			continue ;
 		}
-	    units[count] = str.at(i);
+	    units.push_back(str.at(i));
+	    umap[str.at(i)] = count;
 	    count++;
 	}
+	units.push_back('_');
+	umap['_'] = count;
 
 	infile >> k;
 	string s[k] ;
     for(int i = 0 ; i < k ; i++){
     	infile >> s[i];
+    	int cmp = s[i].length();
+    	if (maxlen < cmp){
+    		maxlen = cmp;
+    	}
     }
+
+    //Storing costs
     infile >> cc ;
 	for(int i = 0 ; i <= m ; i++){
+		vector<int> temp(m+1,0);
 	    for(int j = 0 ; j <= m ; j++){
-	        infile >> costmap[i][j] ;
+	        infile >> temp.at(j);
 	    }
+	    globalcosts.push_back(temp);
 	}
 
-	// cout << time << endl;
-	// cout << m << endl;
-	// cout << k << endl;
-	// for (int i = 0; i < m; i++){
-	// 	cout << units[i] << " ";
+	maxlen += 1;
+
+	state first(maxlen);
+
+	for (int i = 0; i < k; i++){
+		while (s[i].size() < maxlen){
+			s[i] = s[i].append("_");
+		}
+		first.add(s[i]);
+	}
+	first.compute_cost();
+	first.print_state();
+
+	// string test = s[0];
+	// cout << "String initially:" << test << endl;
+	// swap(test[2],test[3]);
+	// cout << "String finally:" << test << endl;
+	greedy_local_search(first);
+	// printf("Program will run for %f seconds\n",total_time*60);
+	// while (true){
+		// cout << "Time passed is: " << (time(NULL) - start_time) << endl;
+		// if (time(NULL) - start_time + 5 < total_time*60 ){
+		// 	break;
+		// }
 	// }
-	// cout << endl;
-	// for(int i = 0 ; i < k ; i++){
- 	//    cout << s[i] << endl ;
- 	// }
 
-
- //    infile >> temp ;
-	// cout << temp << endl;
-	// // cout << "Received Costs" << endl ;
- //    for(int i = 0 ; i <= m ; i++){
- //        for(int j = 0 ; j <= m ; j++){
- //            cout << costmap[i][j] << " " ;
- //        }
- //        cout << endl ;
- //    }
 	infile.close();
 	return 0;
 }
