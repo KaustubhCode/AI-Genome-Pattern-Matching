58c7893e253d0101e6b364a55d3c5bc1974c0889
Abhyuday Bhartiya <bhartiyabhyuday@gmail.com>
2019-08-29 00:21:21 +0530

    Test File Generation Script

---
+++ Source1.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+float RandomFloat(float min, float max)
+{
+	// this  function assumes max > min, you may want 
+	// more robust error checking for a non-debug build
+	assert(max > min);
+	float random = ((float)rand()) / (float)RAND_MAX;
+
+	// generate (in your case) a float between 0 and (4.5-.78)
+	// then add .78, giving you a float between .78 and 4.5
+	float range = max - min;
+	return (random * range) + min;
+}
+
+string generateString(char[] c, int len) {
+	string s = "";
+	int index;
+	for (int j = 0; j < len; j++) {
+		index = rand() % 4;
+		s += c[index];
+	}
+
+	return s;
+}
+
+int main() {
+
+	float f;
+	f = RandomFloat(0.5, 4.0)
+	int k = 4;
+	char c[] = {'A','C','T','G'}
+	int m = 0;
+	while (m == 0) {
+		m = rand() % 21;
+	}
+	string s[m];
+	int maxlen = 0;
+	while (maxlen < 10) {
+		maxlen = rand() % 31;
+	}
+	int len;
+	for (int i = 0; i < m; i++) {
+		len = 0;
+		while (len <= 0.7 * maxlen) {
+			len = rand() % (maxlen + 1);
+		}
+		s[i] = generateString(c, len);
+	}
+	int cc;
+	cc = rand() % 7;
+	int cost[m + 1][m + 1];
+	for (int i = 0; i < m + 1; i++) {
+		cost[i][i] = 0;
+		for (int j = i + 1; j < m + 1; j++) {
+			cost[i][j] = rand() % 10;
+			cost[j][i] = cost[i][j];
+		}
+	}
+	ofstream myfile;
+	myfile.open("E:/example.txt");
+	myfile << f << endl;
+	myfile << k << endl;
+	/*for (int i = 0; i < 3; i++) {
+		myfile << c
+	}*/
+	myfile << "A, C, T, G" << endl;
+	myfile << m << endl;
+	for (int i = 0; i < m; i++) {
+		cout << s[i] << endl;
+	}
+	cout << cc << endl;
+	for (int i = 0; i < m + 1; i++) {
+		for (int j = 0; j < m + 1; j++) {
+			cout << cost[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
